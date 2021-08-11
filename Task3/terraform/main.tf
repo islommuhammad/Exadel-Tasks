ef30b358573812496ef0d7c91eb63f95a5a8814f
islommuhammad <salomshox@gmail.com>
2021-08-11 10:00:20 +0500

    Modules added

--- Task3/terraform/main.tf
+++ Task3/terraform/main.tf
@@ -3,98 +3,9 @@ provider "aws" {
   profile = "MyAWS"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "main"
-  }
-}
-resource "aws_internet_gateway" "gw" {
-    vpc_id = aws_vpc.main.id
-  
-}
-### Routing table
-resource "aws_route_table" "main-route-table" {
-  vpc_id = aws_vpc.main.id
-  route  {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = aws_internet_gateway.gw.id
-    }
-
-  tags = {
-    Name = "main-route"
-  }
-}
-resource "aws_subnet" "subnet-1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "eu-central-1a"
-  tags = {
-    Name = "subnet-1"
-  }
-}
-
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.subnet-1.id
-  route_table_id = aws_route_table.main-route-table.id
-}
-
-### Security group
-resource "aws_security_group" "allow_web" {
-  name        = "allow_web_traffic"
-  description = "Allow WEB inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-      description      = "HTTPS from VPC"
-      from_port        = 443
-      to_port          = 443
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-  
-  ingress {
-      description      = "HTTP from VPC"
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-  ingress {
-      description      = "SSH from VPC"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-
-  egress  {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
-  
-
-  tags = {
-    Name = "allow_web"
-  }
-}
-### Network interface
-resource "aws_network_interface" "web-server-nic" {
-  subnet_id       = aws_subnet.subnet-1.id
-  private_ips     = ["10.0.1.50"]
-  security_groups = [aws_security_group.allow_web.id]
-  
-}
-### Elastic IP address
-resource "aws_eip" "one" {
-  vpc                       = true
-  network_interface         = aws_network_interface.web-server-nic.id
-  associate_with_private_ip = "10.0.1.50"
-  depends_on = [aws_internet_gateway.gw]
+module "networking" {
+  source    = "./modules/networking"
+  #namespace = var.namespace
 }
 
 #### Ubuntu server
@@ -102,13 +13,24 @@ resource "aws_instance" "web-server" {
   ami           = "ami-0b1deee75235aa4bb"
   instance_type = "t2.micro"
   key_name      = "webserver-key"
-  network_interface {
-    device_index = 0
-    network_interface_id = aws_network_interface.web-server-nic.id
-  }
-  user_data     = "${file("installweb.sh")}"
+  subnet_id     = module.networking.vpc.public_subnets[0]
+  vpc_security_group_ids  = [module.networking.sg_pub_id]
+  user_data = "${file("installweb.sh")}"
   tags = {
     Name = "Ubuntu-server"
+  }
+}
+
+  #### CentOS server
+resource "aws_instance" "centos-server" {
+  ami           = "ami-0005dd1bdf84d89d0"
+  instance_type = "t2.micro"
+  key_name      = "webserver-key"
+  subnet_id     = module.networking.vpc.private_subnets[0]
+  vpc_security_group_ids  = [module.networking.sg_priv_id]
+  #user_data     = "${file("installweb.sh")}"
+  tags = {
+    Name = "CentOS7-server"
   }
 
 }
